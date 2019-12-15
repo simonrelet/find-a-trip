8238cbd5d1073d43cbf31e76a764a056816e1920
Simon Relet <simon.relet@gmail.com>
2019-12-15 23:35:14 +0100

    chore cleaning

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import { App } from './App';
 import { ErrorBoundary } from './ErrorBoundary';
 
+global.APP_VERSION = process.env.REACT_APP_VERSION;
+global.APP_NAME = process.env.REACT_APP_NAME;
+
 ReactDOM.render(
   <React.StrictMode>
     <ErrorBoundary>
